6326020ffdd419dd43e06b135ecd1ce02abfb161
Debashis121212 <139324072+Debashis121212@users.noreply.github.com>
2023-12-28 11:03:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go!', 
+    success: 'Here you go!' message.user.number, 
 }
 
 let file = require.resolve(__filename)
